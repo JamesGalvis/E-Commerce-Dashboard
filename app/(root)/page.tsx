55cb0ec40ebe1ce897b862e14f91f0db3da95f58
James Galvis <jamesrgal@gmail.com>
2023-09-30 20:40:08 -0500

    Environment Setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1>Admin Dashboard</h1>
+      <Button>Click me</Button>
+    </main>
+  )
+}
